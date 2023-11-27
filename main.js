9be35b5c72d4958ef986e5fc835b5c811e7607f2
João Henrique <108554606+JonhenRed@users.noreply.github.com>
2023-11-27 11:03:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,51 @@ function tocarSomAplausos(){
     document.querySelector('#som_tecla_aplausos').play();
 }
 document.querySelector('.tecla_aplausos').onclick = tocarSomAplausos;
+
+//Som de vaia
+function tocarSomVaia(){
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_vaia').onclick = tocarSomVaia;
+
+//Som de risada
+function tocarSomRisada(){
+    document.querySelector('#som_tecla_risada').play();
+}
+document.querySelector('.tecla_risada').onclick = tocarSomRisada;
+
+//Som de errado
+function tocarSomErrado(){
+    document.querySelector('#som_tecla_errado').play();
+}
+document.querySelector('.tecla_errado').onclick = tocarSomErrado;
+
+//Som de grilo
+function tocarSomGrilo(){
+    document.querySelector('#som_tecla_grilo').play();
+}
+document.querySelector('.tecla_grilo').onclick = tocarSomGrilo;
+
+//Som de tambores
+function tocarSomTambores(){
+    document.querySelector('#som_tecla_tambores').play();
+}
+document.querySelector('.tecla_tambores').onclick = tocarSomTambores;
+
+//Som de Trombeta
+function tocarSomTrombeta(){
+    document.querySelector('#som_tecla_trombeta').play();
+}
+document.querySelector('.tecla_trombeta').onclick = tocarSomTrombeta;
+
+//Som de dinheiro
+function tocarSomDinheiro(){
+    document.querySelector('#som_tecla_dinheiro').play();
+}
+document.querySelector('.tecla_dinheiro').onclick = tocarSomDinheiro;
+
+//Som de Vitoria
+function tocarSomVitoria(){
+    document.querySelector('#som_tecla_vitoria').play();
+}
+document.querySelector('.tecla_vitoria').onclick = tocarSomVitoria;
